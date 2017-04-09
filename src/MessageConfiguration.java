f48721f0819797bc6cd28524b91b7390014dbe57
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2017-04-09 13:36:32 -0400

    added constants for handshake message

---
+++ src/MessageConfiguration.java
@@ -0,0 +1,11 @@
+/**
+ * Created by radhikadesai on 09/04/2017.
+ */
+public class MessageConfiguration {
+    //Handshake config
+    public static final int HANDSHAKE_MSG_LEN = 32;
+    public static final int HANDSHAKE_HEADER_LEN = 18;
+    public static final int HANDSHAKE_ZERO_LEN = 10;
+    public static final int HANDSHAKE_PEERID_LEN = 4;
+    public static final String HANDSHAKE_HEADER = "P2PFILESHARINGPROJ";
+}
