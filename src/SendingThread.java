cd5009ca5f8424f6bb0b464d579b987c96ff6156
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2017-04-09 16:40:35 -0400

    Sending handshake via sendingThread

--- src/SendingThread.java
+++ src/SendingThread.java
@@ -15,25 +15,25 @@ public class SendingThread implements Runnable {
 
     private int connectionType;
     private Socket peerSocket = null;
-    private InputStream in;
-    private OutputStream out;
+    private InputStream inputStream;
+    private OutputStream outputStream;
     private Handshake handshakeMessage;
-    private int ownPeerId;
+    private int myPeerId;
     private int remotePeerId;
 
     public SendingThread(Socket peerSocket, int ownPeerID) {
 
         this.peerSocket = peerSocket;
         this.connectionType = 0;
-        this.ownPeerId = ownPeerID;
+        this.myPeerId = ownPeerID;
         try
         {
-            in = peerSocket.getInputStream();
-            out = peerSocket.getOutputStream();
+            inputStream = peerSocket.getInputStream();
+            outputStream = peerSocket.getOutputStream();
         }
         catch (Exception ex)
         {
-//            PeerProcess.showLog(this.ownPeerId + " Error : " + ex.getMessage());
+//            PeerProcess.showLog(this.myPeerId + " Error : " + ex.getMessage());
         }
     }
     public SendingThread(String add, int port,int ownPeerID)
@@ -41,8 +41,8 @@ public class SendingThread implements Runnable {
         try
         {
             this.connectionType = 1;
-            this.ownPeerId = ownPeerID;
-            //PeerProcess.showLog(ownPeerId + " Receiving Port = " + port + " Address = "+ add);
+            this.myPeerId = ownPeerID;
+            //PeerProcess.showLog(myPeerId + " Receiving Port = " + port + " Address = "+ add);
             this.peerSocket = new Socket(add, port);
         }catch (UnknownHostException e)
         {
@@ -53,15 +53,61 @@ public class SendingThread implements Runnable {
         }
         try
         {
-            in = peerSocket.getInputStream();
-            out = peerSocket.getOutputStream();
+            inputStream = peerSocket.getInputStream();
+            outputStream = peerSocket.getOutputStream();
         }catch (Exception ex)
         {
 //            PeerProcess.showLog(ownPeerID + " RemotePeerHandler : " + ex.getMessage());
         }
     }
+    public boolean sendHandshake(){
+        try
+        {
+            outputStream.write(Handshake.sendMessage(this.myPeerId));
+        }
+        catch (IOException e)
+        {
+//            PeerProcess.showLog(this.myPeerId + " SendHandshake : " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void run() {
+        boolean handshakeSuccess = sendHandshake();
+
+        if(handshakeSuccess){
+            //peerProcess.showLog(ownPeerId + " HANDSHAKE has been sent...");
+        }
+        else {
+//            peerProcess.showLog(ownPeerId + " HANDSHAKE has been failed...");
+            System.exit(0);
+        }
+        //Read handshake message
+        while (true){
+            try {
+                byte[] message = new byte[Handshake.MESSAGE_LENGTH];
+                //Read handshake message
+                inputStream.read(message);
+                Handshake.receiveMessage(message);
+                if(Handshake.received_header.equals(Handshake.HEADER)){
+                    remotePeerId = Integer.parseInt(Handshake.received_peerID);
+                    // Log
+                    //peerID to socket mapping
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(this.connectionType==ACTIVE){
+            //Send bitfield and change remotepeer state to 8
+        }
+        else{
+            //set remotePeer state to 2
+        }
+        
 
     }
 }
