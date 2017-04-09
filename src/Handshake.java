3ffffbd3576488dc2b8dbdb9344bfb9738c49178
jainayushi <ayushijain@ufl.edu>
2017-04-09 01:01:39 -0400

    Added logic for verifying received handshake message

--- src/Handshake.java
+++ src/Handshake.java
@@ -8,7 +8,8 @@ public class Handshake {
 	public static final int ID_LEN= 4;
 	public static final int MESSAGE_LENGTH = 32;
 	public static final String HEADER= "P2PFILESHARINGPROJ";
-	
+	public static String received_header;
+	public static String received_peerID;
 	public Handshake()
 	{
 		
@@ -35,5 +36,31 @@ public class Handshake {
 		
 	}
 	
-
+	public static int verifyHandshake(int peer)
+	{
+		int verified = 0;
+		if (received_peerID == Integer.toString(peer) && received_header.equals(HEADER))
+			verified =1;
+		return verified;
+	}
+	
+	public static void receiveMessage(byte[] message)
+	{
+		byte[] msgHeader = new byte[HEADER_LEN];
+		byte[] id = new byte[ID_LEN];
+		try
+		{
+		if(message.length!= MESSAGE_LENGTH)
+			throw new Exception("Handshake message length incorrect");
+		System.arraycopy(message, 0, msgHeader, 0, HEADER_LEN);
+		received_header = new String(msgHeader);
+		System.arraycopy(message, HEADER_LEN + ZERO_BITS_LEN, id, 0, ID_LEN);
+		received_peerID = new String(id);
+		
+		}
+		catch (Exception e) {
+			
+		}
+		
+	}
 }
