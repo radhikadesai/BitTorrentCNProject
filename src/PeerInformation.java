29045039be61caa0e519eb8e330d11bde6b256a2
dhruvmhjn <dhruvmhj@gmail.com>
2017-04-11 15:16:54 -0400

    Changes in peer information

--- src/PeerInformation.java
+++ src/PeerInformation.java
@@ -1,26 +1,44 @@
+import java.util.Date;
 /**
  * Created by radhikadesai on 08/04/2017.
  */
-public class PeerInformation {
+
+public class PeerInformation implements Comparable<PeerInformation> {
     int peerID;
     String address;
     int port;
     int isFirstPeer;
     int index;
+    public double dataRate = 0;
+	public int isInterested = 1;
+	public int isPNeighbor = 0;
+	public int isOUNeighbor = 0;
+	public int isCh = 1;
+	public BitField bitField;
+	public int state = -1;
+	public int peerIndex;
+	public Date st;
+	public Date ft;
+	public int isCompleted = 0;
+    public int isHandShaked = 0;
 
-    public int getPeerID() {
+    public int getPeerID() 
+    {
         return peerID;
     }
 
-    public void setPeerID(int peerID) {
+    public void setPeerID(int peerID)
+    {
         this.peerID = peerID;
     }
 
-    public int getIndex() {
+    public int getIndex()
+    {
         return index;
     }
 
-    public void setIndex(int index) {
+    public void setIndex(int index)
+    {
         this.index = index;
     }
 
@@ -47,5 +65,15 @@ public class PeerInformation {
     public void setIsFirstPeer(int isFirstPeer) {
         this.isFirstPeer = isFirstPeer;
     }
+	public int compareTo(PeerInformation o1) 
+	{
+		
+		if (this.dataRate > o1.dataRate) 
+			return 1;
+		else if (this.dataRate == o1.dataRate) 
+			return 0;
+		else 
+			return -1;
+	}
 }
 
