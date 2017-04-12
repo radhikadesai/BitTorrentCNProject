7f2943b4eb5275abc35f09e5515baa74cea4c09a
Radhika Desai <radhikadesai@Radhikas-MacBook-Pro.local>
2017-04-11 22:16:52 -0400

    removed getter setter for start and end

--- src/PeerInformation.java
+++ src/PeerInformation.java
@@ -28,22 +28,7 @@ public class PeerInformation implements Comparable<PeerInformation> {
     {
         return peerID;
     }
-
-    public Date getStart() {
-        return start;
-    }
-
-    public void setStart(Date start) {
-        this.start = start;
-    }
-
-    public Date getEnd() {
-        return end;
-    }
-
-    public void setEnd(Date end) {
-        this.end = end;
-    }
+    
 
     public void setPeerID(int peerID)
     {
